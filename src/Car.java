07b7dd4fe3eba4319ee1f2ee1039b8b3f985ca8f
LG <LG@DESKTOP-M3BEAC6>
2020-04-07 22:24:19 +0900

    4.7 subject

---
+++ src/Car.java
@@ -0,0 +1,22 @@
+
+public class Car {
+
+	int year;
+	String maker;
+	double speed;
+	String source;
+	String price;
+	
+	public void Constructor() {
+		this.year = year;
+		this.maker = maker;
+		this.speed = speed;
+		
+	}
+	public void GetterMethods() {
+		public int getYear() {
+			 return this.year;
+		}
+	}
+	
+}
---
+++ src/GuessCoin.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class GuessCoin {
+	
+	private static final String H = null;
+
+	public static void main(String[] args) {
+		double randomDouble = Math.random();
+		int randomInt = (int) (randomDouble*2)+1;
+		Scanner input = new Scanner(System.in);
+		System.out.println("H or T?:");
+		String answer = input.nextLine();
+				
+		
+		if (answer == H) {
+			int y = 1;
+			if (randomInt == y) {
+				System.out.println("Correct!");
+			}
+			else {
+				System.out.println("InCorrect!");
+			}
+			
+		}
+		else {
+			int y = 2;
+			if (randomInt == y) {
+				System.out.println("Correct!");
+			}
+			else {
+				System.out.println("InCorrect!");
+			}
+		}
+
+		
+	}
+
+}
